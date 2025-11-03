03560fd4c55eed4f9f15d79752ded8f491b93c3e
Kavin-Jayaprakash <kavinjp486@gmail.com>
2025-11-03 10:06:28 +0530

    Intial commit

---
+++ src/main/java/in/kavininfra/kavininfra_demo/KavininfraDemoApplication.java
@@ -0,0 +1,13 @@
+package in.kavininfra.kavininfra_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KavininfraDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KavininfraDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/kavininfra/kavininfra_demo/KavininfraDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package in.kavininfra.kavininfra_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KavininfraDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
